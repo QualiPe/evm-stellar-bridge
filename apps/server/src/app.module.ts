b82cb7629f0c463758144f32f5291f2b684a88d8
QualiPe <qualipech@gmail.com>
2025-07-31 10:52:13 +0500

    feat: add module validations

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './shared/config.module';
+import { ResolverModule } from './modules/resolver/resolver.module';
+import { IntentModule } from './modules/intent/intent.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    ResolverModule,
+    IntentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
